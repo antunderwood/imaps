62d59c7bfa4172c43eeea4c14fc0be5470bd12ee
Anthony Underwood <email2ants@gmail.com>
2010-04-10 21:27:17 +0100

    first attempt at map

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,8 @@
+class MapsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,24 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// JQtouch
+var jQT = new $.jQTouch({
+    icon: 'jqtouch.png',
+    addGlossToIcon: false,
+    startupScreen: 'jqt_startup.png',
+    statusBar: 'black'
+});
+
+var osMap;
+function init_map()
+{
+  osMap = new OpenSpace.Map('map');
+  var lonlat = new OpenLayers.LonLat(-0.3879547119140625, 51.6934690476718);
+  var gridProjection = new OpenSpace.GridProjection();
+  var pos = gridProjection.getMapPointFromLonLat(lonlat);
+  osMap.setCenter(pos, 8);
+
+  var markers = new OpenLayers.Layer.Markers("Markers");
+  osMap.addLayer(markers);
+
+  var marker;
+  marker = new OpenLayers.Marker(pos);
+  markers.addMarker(marker);
+}
